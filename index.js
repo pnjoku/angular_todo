768afedcb8de4ac8941be6761c10614bae246f84
Prince <princenjoku55@gmail.com>
2016-07-29 01:01:10 -0400

    basic app things, updated dependencies

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-console.log('hello world')
+'use strict';
+
+const app = require('express')();
+const bodyParser = require('body-parser');
+
+const port = 8585;
+const todos = [];
+
+// start the api
+app.listen(port, x => {
+
+  console.log("todoApp Started");
+});
+
+
+app.use(bodyParser.json());
+
+app.all('/', (req, res) => res.send("<h1>Welcome to my todoApp</h1>"));
