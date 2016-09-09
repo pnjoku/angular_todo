f34cdcb51441cbab868ba50b2433358886ba6992
Prince Njoku <pnjoku@pnjoku-mbp.local>
2016-09-09 14:17:29 -0400

    added remaining and placeholder

--- templates/todoController.js
+++ templates/todoController.js
@@ -5,11 +5,21 @@ angular
     todoList.todos = [{'title':'Build a todo app','done': false}]
     todoList.addTodo =  addTodo
     todoList.clearCompleted = clearCompleted
+    todoList.remaining = remaining
 
     function addTodo(){
       todoList.todos.push({'title': todoList.newTodoTitle, 'done':false})
       todoList.newTodoTitle = ""
     }
+
+    function remaining(){
+    var count = 0
+    angular.forEach(todoList.todos, function(todo) {
+      count += todo.done ? 0 : 1
+    })
+    return count
+   }
+
     function clearCompleted(){
       todoList.todos = todoList.todos.filter(function(item){
         return ! item.done
